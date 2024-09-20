ac98d0bb8794ea139d35ffbb1fc74ed0ba6af103
enzoFava <117693440+enzoFava@users.noreply.github.com>
2024-09-20 13:11:09 -0300

    lint error

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,14 +30,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    ('https://enzofava1997-8000.'
-     'theiadockernext-0-labs-prod-'
-     'theiak8s-4-tor01.proxy.cognitiveclass.ai'),
+    (
+        'https://enzofava1997-8000.'
+        'theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
 ]
 CSRF_TRUSTED_ORIGINS = [
-    ('https://enzofava1997-8000.'
-    'theiadockernext-0-labs-prod-'
-    'theiak8s-4-tor01.proxy.cognitiveclass.ai')
+    (
+        'https://enzofava1997-8000.'
+        'theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
 ]
 
 REST_FRAMEWORK = {
